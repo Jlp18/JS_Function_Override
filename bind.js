95bbd89cbb740b98b52d4feb0ea92b29ee908bd6
eJlp18 <517602852@qq.com>
2019-08-19 22:36:55 +0800

    bind方法修改

--- bind.js
+++ bind.js
@@ -3,7 +3,6 @@ Function.prototype.mybind = function(context) {
     if(typeof this !== 'function'){
         throw new TypeError('not function');
     }
-    context = context || window;
     let _this = this;
     let args = [...arguments].slice(1);
     return function F(){
