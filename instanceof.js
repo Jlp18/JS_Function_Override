814fc3ddf788dd6568e83d13bf7ea733ec697e1c
eJlp18 <517602852@qq.com>
2019-08-20 21:35:38 +0800

    instanceof方法

---
+++ instanceof.js
@@ -0,0 +1,14 @@
+// 左边的变量的原型链上是否存在右边变量的原型
+function instanceOf(left, right){
+    let leftValue = left.__proto__;
+    let rightValue = right.prototype;
+    while(true){
+        if(leftValue === null){
+            return false;
+        }
+        if(leftValue === right){
+            return true;
+        }
+        leftValue = rightValue.__proto__;
+    }
+}
