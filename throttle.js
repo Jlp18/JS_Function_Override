0c1f871517f3548f68b8995cf5b816ac62d15baf
eJlp18 <517602852@qq.com>
2019-08-21 23:03:32 +0800

    节流函数

---
+++ throttle.js
@@ -0,0 +1,14 @@
+// 实现一个节流函数 限制函数触发频率
+function throttle(action, delay){
+    // 利用闭包保存时间
+    let prev = Date.now();
+    return function(){
+        let now = Date.now();
+        let context = this;
+        let args = arguments;
+        if(now - prev >= delay){
+            action.apply(context, args);
+            prev = Date.now();
+        }
+    }
+}
